fddfe1f443bef63f40a0f2ec765879c95495fb1d
Dhathrineerukonda <143919208+Dhathrineerukonda@users.noreply.github.com>
2024-07-05 13:14:35 +0530

    Background Image added

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -16,7 +16,7 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a image you
  * freely use on your site.
  */
-import image from "../images/flowers.jpg";
+import image from "../images/flowers-picture.jpg";
 
 const imageAltText = "flowers with black ground";
 
